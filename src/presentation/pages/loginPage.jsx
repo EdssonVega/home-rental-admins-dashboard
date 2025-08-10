ed4d1fb7b8b3d6e6f5ab7f3d916f15bc1658e850
Edsson <e_d_sx2@hotmail.com>
2025-08-10 18:28:29 -0400

    finished login screen

---
+++ src/presentation/pages/loginPage.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { loginFailure, loginRequest, loginSucces } from '../../state/auth/authSlice';
+import LoginForm from '../components/loginForm';
+
+export const LoginPage = () => {
+
+  const dispatch = useDispatch();
+  const {loading, error, isAuthenticated} = useSelector((state)=> state.auth);
+
+  const handleLogin = (data) => {
+    dispatch(loginRequest());
+
+    setTimeout (()=> {
+      if(data.email === "edssonvega@gmail.com" && data.password === "12345678"){
+        dispatch(loginSucces({email: data.email}));
+        alert("Inicio de sesión exitoso!")
+      } else {
+        dispatch(loginFailure("Correo o contraseña incorrectos"));
+        alert("Correo o contraseña incorrectos");
+      }
+    },1000);
+  };
+
+  if(isAuthenticated){
+    return <div>!Bienvenido de nuevo!</div>;
+  }
+
+  return (
+    <div className='login-page'>
+      <h2>Iniciar sesión</h2>
+      {error && <p style={{color: "red"}}>{error}</p>}
+      <LoginForm onSubmit={handleLogin} loading={loading}/>
+    </div>
+  )
+}
+
+export default LoginPage;
